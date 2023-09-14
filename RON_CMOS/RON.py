4808fae608a25a05e4e7966c35711ccd2d162b8b
Aidan MacNichol <aidanmacnichol11@gmail.com>
2023-09-14 15:55:11 -0700

    Updated Code

--- RON_CMOS/RON.py
+++ RON_CMOS/RON.py
@@ -7,13 +7,10 @@ import os
 
 import loadImage
 
-plt.style.use(astropy_mpl_style)
+#plt.style.use(astropy_mpl_style)
 
 # Name of folder containing images
 relativePath = '12.8C' 
-
-# constant offset so pixel values are non-zero after subtraction
-offset = 10
 
 # get path of image folder
 absPath = os.path.dirname(__file__)
@@ -24,39 +21,61 @@ fitsLoader = loadImage.fitsLoader(fullPath)
 #load Image
 fitsLoader.loadImages()
 
-# Subtract N frames from eachother
-stackedImg = fitsLoader.stackFrames()
+#------------------------------------------------------------
 
-fitsLoader.test()
+# create np array same shape different type
+stdArray = np.empty_like(fitsLoader.images[0], dtype=np.float64)
+pixelArray = np.empty(10, dtype=np.int32)
+total = 0
 
-# print(type(stackedImg))
-# print(stackedImg[1][1])
-# print(stackedImg[2][3])
+"""
+returns a 1D array of length N
+Contains the pixel value at a (x,y) coordinate for N frames
+"""
+def retrievePixels(x, y):
+    for i, img in enumerate(fitsLoader.images):
+        newElement = img[y][x]
+        pixelArray[i] = newElement
+    return pixelArray 
 
-
-#may need to copy values to an array of uint32 elements
-stackedOffset = stackedImg + offset
-
-
-
-#print for testing
-print(f"no offset: \n{stackedImg[1:6,1:6]}")
-print(f"w/ offset: \n{stackedOffset[1:6,1:6]}")
-print(f"min: {np.min(stackedOffset)}")
-print(f"max {np.max(stackedOffset)}")
-
-
-stdev = np.std(stackedOffset)
-final = stdev/math.sqrt(2) 
-
-print(final) 
+"""
+find rms of pixel array
+"""
+def calcRMS(arr):               # Sqrt(N)? 
+    return np.std(arr) / math.sqrt(2)  
 
 
 
-# # print(image_data.shape)
+def calcPixelRMS():
+    rows, cols = fitsLoader.images[0].shape
+    for y in range(rows - 1):
+        for x in range(cols - 1):
+            pix = retrievePixels(x,y)
+            rms = calcRMS(pix) 
+            stdArray[y][x] = rms 
+    return stdArray
 
-# fig, (ax0, ax1) = plt.subplots(nrows=2)
+stdArray = calcPixelRMS()
 
-# # plt.imshow(data2, cmap='gray')
+print(np.min(stdArray))
+print(np.max(stdArray))
 
-# # 
+
+fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5))
+
+heatmap = ax1.imshow(stdArray, cmap='jet', aspect='auto')
+ax1.set_title('RMS Heatmap')
+plt.colorbar(heatmap, ax=ax1)
+
+stdArray_flat = stdArray.flatten()
+
+ax2.hist(stdArray_flat, bins=200)
+ax2.set_title('RMS Histogram')
+ax2.set_xlabel('RMS Values (ADU)')
+ax2.set_ylabel('log(count)')
+ax2.set_yscale('log')
+
+plt.tight_layout()
+plt.show() 
+
+
